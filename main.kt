62bd06e5607434360eccd58c7e32184ac6e51644
GitProger <alex0surname@gmail.com>
2021-08-27 23:24:28 +0300

    main

--- main.kt
+++ main.kt
@@ -1,3 +1,19 @@
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.SwingUtilities
+
+class HelloWorld : Runnable {
+    override fun run() {
+        val f = JFrame("Hello, World!")
+        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE)
+        f.add(JLabel("Hello World"))
+        f.pack()
+        f.setVisible(true)
+    }
+}
+
 fun main() {
+    SwingUtilities.invokeLater(HelloWorld())
+
     println("Hello")
 }
