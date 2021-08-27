3360a2505b1c755e78c67b5be5e27dd5937dff6b
Dmitry <mitya20-04@yandex.ru>
2021-08-28 01:24:01 +0300

    ButtonPanel

--- client/ButtonPanel.kt
+++ client/ButtonPanel.kt
@@ -8,30 +8,24 @@ import kotlin.system.exitProcess
 
 object UnwrappedButtonPanel : JPanel(GridLayout(10, 1)) {
     init {
-        val roomField = JTextField()
-        roomField.addCaretListener{
-            room = roomField.text
-            KettleCanvas.repaint()
-        }
-
         val dontWantTea = JButton("I don't want tea!")
-        dontWantTea.addActionListener {exitProcess(0) }
+        dontWantTea.addActionListener { exitProcess(0) }
         add(dontWantTea)
 
         val addKettle = JButton("Add new kettle")
-        addKettle.addActionListener {  addNewKettle() }
+        addKettle.addActionListener { addNewKettle() }
         add(addKettle)
 
         val removeKettle = JButton("Remove selected kettle")
-        removeKettle.addActionListener {  removeSelectedKettle() }
+        removeKettle.addActionListener { removeSelectedKettle() }
         add(removeKettle)
 
         val boilKettle = JButton("Boil selected kettle")
-        boilKettle.addActionListener {  boilSelectedKettle() }
+        boilKettle.addActionListener { boilSelectedKettle() }
         add(boilKettle)
 
         val drinkSome = JButton("Drink some water from selected kettle")
-        drinkSome.addActionListener {  drinkSomeWater() }
+        drinkSome.addActionListener { drinkSomeWater() }
         add(drinkSome)
     }
 }
@@ -45,18 +39,14 @@ fun drinkSomeWater() {
         try {
             val intVolume = it[0].toInt()
 
-            if (intVolume <= 0) JOptionPane.showInternalMessageDialog(
-                dialog,
+            if (intVolume <= 0) {
                 "Volume is expected to be positive! (Did you drink more than was?)"
-            )
-            drink(KettleCanvas.getSelectedKettle().id, intVolume)
-            true
+            } else {
+                drink(KettleCanvas.getSelectedKettle().id, intVolume)
+                null
+            }
         } catch (e: NumberFormatException) {
-            JOptionPane.showInternalMessageDialog(
-                dialog,
-                "Volume is expected to be integer (No one can measure so accurately)"
-            )
-            false
+            "Volume is expected to be integer (No one can measure so accurately)"
         }
     }
     KettleCanvas.repaint()
@@ -72,42 +62,24 @@ fun boilSelectedKettle() {
         "Boil?",
         "How much did you pour? (In millilitres)"
     )
-    do {
-        val state = dialog.waitForRes()
-        if (state == -1) return
-        val (volume) = dialog.getResult()
-        var ok = false
-
+    dialog.processData {
         try {
-            val intVolume = volume.toInt()
+            val intVolume = it[0].toInt()
 
-            if (intVolume <= 0) JOptionPane.showInternalMessageDialog(
-                dialog,
+            if (intVolume <= 0) {
                 "Volume is expected to be positive! (Do you want to donate water?)"
-            )
-            boilKettle(KettleCanvas.getSelectedKettle().id, intVolume)
-            ok = true
+            } else {
+                boilKettle(KettleCanvas.getSelectedKettle().id, intVolume)
+                null
+            }
         } catch (e: NumberFormatException) {
-            JOptionPane.showInternalMessageDialog(
-                dialog,
-                "Volume is expected to be integer (No one can measure so accurately)"
-            )
-            dialog.reject()
+            "Volume is expected to be integer (No one can measure so accurately)"
         }
-    } while (ok)
+    }
     KettleCanvas.repaint()
 }
 
 fun addNewKettle() {
-    val dialog = MultiEnterDialog(
-        "We need more kettles!",
-        "Where are you? (Enter your room number)"
-    )
-    val state = dialog.waitForRes()
-    if (state == -1) return
-    val (room) = dialog.getResult()
-
     addKettle(room)
-    KettleCanvas.repaint()
 }
 
